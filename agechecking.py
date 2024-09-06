eb06742efa5ecbcf7b93b876975bef61c75f07cc
JonathanParra467 <jparra1786@students.mchenry.edu>
2024-09-06 18:37:50 -0500

    Add files via upload

---
+++ agechecking.py
@@ -0,0 +1,18 @@
+age = int(input("whats your age?: "))
+print(age)
+if age >= 16:
+    print("you are old enough to drive")
+else:
+    print("you are not old enough to drive")
+if age >= 18:
+    print("you are old enough to vote")
+else:
+    print("you are not old enough to vote")
+if age >= 21:
+    print("you are old enough to drink alcohol")
+else:
+    print("you are not old enough to drink alcohol")
+if age >= 65:
+    print("you are old enough to be elegible for a senior discount")
+else:
+    print("you are not old enough to be elegible for a senior discount")
